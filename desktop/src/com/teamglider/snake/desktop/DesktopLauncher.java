f30d7b4121f769b02b89086ffba077d446483ae9
Guillaume Quittet <guillaume.quittet@gmail.com>
2016-12-14 15:01:28 +0100

    Change html resolution

--- desktop/src/com/teamglider/snake/desktop/DesktopLauncher.java
+++ desktop/src/com/teamglider/snake/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Snake";
-		config.height = 640;
 		config.width = 360;
+		config.height = 640;
 		new LwjglApplication(new Snake(), config);
 	}
 }
